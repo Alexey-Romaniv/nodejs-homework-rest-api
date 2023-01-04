7aa394b137bd624df0e096269413bdf1640bbf91
Alexey-Romaniv <105770723+Alexey-Romaniv@users.noreply.github.com>
2023-01-04 22:38:48 +0200

    made 4th hw

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,11 +1,11 @@
 const { HttpError } = require("../helpers");
 const validateBody = (schema) => {
-  console.log("111");
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
       next(HttpError(400, error.message));
     }
+    next();
   };
   return func;
 };
