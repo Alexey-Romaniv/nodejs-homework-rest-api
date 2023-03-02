6dfc636a618f030f0e92107c09e35e2ae4802024
Alexey-Romaniv <105770723+Alexey-Romaniv@users.noreply.github.com>
2023-03-02 12:54:09 +0100

    delete path "/api"

--- app.js
+++ app.js
@@ -18,12 +18,12 @@ app.use("/users", userRouter);
 app.use("/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
+    res.status(404).json({message: "Not found"});
 });
 
 app.use((err, req, res) => {
-  const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message });
+    const {status = 500, message = "Server error"} = err;
+    res.status(status).json({message});
 });
 
 module.exports = app;
